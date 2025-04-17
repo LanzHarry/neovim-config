9418ef5cb8977feac717a8ccccad8c6d7ad6bcf1
Harry Lanz <lanzharry97@gmail.com>
2025-04-17 20:52:32 +0100

    add lualine plugin

---
+++ lua/plugins/core.lua
@@ -0,0 +1,17 @@
+-- this file is for loading core plugins for neovim
+return {
+    -- lualine plugin for custom statusline
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy", -- loads after UI is ready
+    dependencies = { "nvim-tree/nvim-web-devicons" }, -- optional but recommended
+    config = function()
+    require("lualine").setup {
+        options = {
+        theme = "auto",            -- matches your current colorscheme
+        -- section_separators = "",   -- optional cleaner look
+        -- component_separators = "",
+      },
+    }
+  end,
+}
+
