26aec54db5f584f85137437eb44b9b3456a0ce8b
Harry Lanz <lanzharry97@gmail.com>
2025-04-23 11:55:37 +0100

    add page up and down keymaps with centering

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- leader and local leader are set already in lazy.lua for correct loading order
-vim.keymap.set("n", "Q", "<nop>")
 
+-- navigation remaps
+vim.keymap.set("n", "<C-d>", "<C-d>zz") -- page down and centre cursor
+vim.keymap.set("n", "<C-u>", "<C-u>zz") -- page up and centre cursor
+
+-- misc keymaps
+vim.keymap.set("n", "Q", "<nop>") -- ensure that pressing Q does not enter the cursed mode
